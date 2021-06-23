9a79b7e5783cc080c0b120d2df2bb2784c2b5ac3
Xiaofeng Wang <henrywangxf@me.com>
2021-06-23 21:40:20 +0800

    aws: Add RHEL 8.5 runner for x86_64 and aarch64
    ami-0da1ac7dca595deca - x86_64
    ami-035a1bdfbb692f195 - aarch64

---
+++ aws/rhel-8.5-x86_64/main.tf
@@ -0,0 +1,18 @@
+module "aws" {
+  source = "../_base"
+
+  name             = "rhel-8.5-x86_64"
+  ami              = "ami-0da1ac7dca595deca"
+  instance_type    = "c5.large"
+  internal_network = var.internal_network
+}
+
+variable "internal_network" {
+  type        = bool
+  description = "Whether this instance should be in the internal network (default: false)."
+  default     = false
+}
+
+output "ip_address" {
+  value = module.aws.ip_address
+}
