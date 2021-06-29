a42fd609d694e6b133fa4391b6a0a17e14e9f291
Ondřej Budai <ondrej@budai.cz>
2021-06-29 15:35:33 +0200

    rename rhel-8.5 to 8.5-devel

--- openstack/rhel-8.5-devel-x86_64/main.tf
+++ openstack/rhel-8.5-devel-x86_64/main.tf
@@ -1,7 +1,7 @@
 module "openstack" {
   source = "../_base"
 
-  name      = "rhel-8-5"
+  name      = "rhel-8-5-devel"
   image_id  = "0a96f6b9-6b53-4b1c-b618-c0aa8f2d5d3d"
   flavor_id = "893c20cf-d5ea-4c7d-9eee-2bc4b3e5723e"
 }
