28032645bcd6e9cd0c95f9a307ec95575495d094
Jakub Rusz <jrusz@redhat.com>
2021-09-02 16:29:52 +0200

    openstack: update rhel-9 guest images
    There seems to be the same problem like we had with aws rhel-9
    guest images, so updating in openstack as well.

--- openstack/rhel-9.0-beta-nightly-x86_64/main.tf
+++ openstack/rhel-9.0-beta-nightly-x86_64/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name     = "rhel-9-0-beta-nightly"
-  image_id = "43a68379-5468-490d-a51a-141ce33f9165"
+  image_id = "c332bd07-2b10-4de9-9090-736e904d09a8"
 }
 
 output "ip_address" {
