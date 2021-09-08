93559ac8f9258dbe91ad52c264a5c918ef6ca718
Jakub Rusz <jrusz@redhat.com>
2021-09-08 10:00:29 +0200

    openstack: add large RHEL-8.5 runner
    We need a more powerful runner for some ostree tests. This also enables
    to define a different flavor for each runner.

--- openstack/rhel-8-x86_64/main.tf
+++ openstack/rhel-8-x86_64/main.tf
@@ -1,8 +1,9 @@
 module "openstack" {
   source = "../_base"
 
-  name     = "rhel-8-3"
-  image_id = "6f56aa22-5ab8-4f8c-84e7-b300d107aed2"
+  name      = "rhel-8-3"
+  image_id  = "6f56aa22-5ab8-4f8c-84e7-b300d107aed2"
+  flavor_id = "893c20cf-d5ea-4c7d-9eee-2bc4b3e5723e"
 }
 
 output "ip_address" {
--- openstack/rhel-8.4-x86_64/main.tf
+++ openstack/rhel-8.4-x86_64/main.tf
@@ -1,8 +1,9 @@
 module "openstack" {
   source = "../_base"
 
-  name     = "rhel-8-4"
-  image_id = "7cf88658-58e2-429e-96a2-385d60f5f1fe"
+  name      = "rhel-8-4"
+  image_id  = "7cf88658-58e2-429e-96a2-385d60f5f1fe"
+  flavor_id = "893c20cf-d5ea-4c7d-9eee-2bc4b3e5723e"
 }
 
 output "ip_address" {
