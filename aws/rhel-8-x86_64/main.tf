2e4d6e908a7eac1487003779f4d20be3ed4af9bf
Ondřej Budai <ondrej@budai.cz>
2021-10-05 12:13:17 +0200

    bring back rhel-8 runners temporarily
    We need them to unblock https://github.com/osbuild/osbuild/pull/835
    
    This partially reverts commit d36255fe83688d2299a7b717291bda20d0b8d634.

---
+++ aws/rhel-8-x86_64/main.tf
@@ -0,0 +1,18 @@
+module "aws" {
+  source = "../_base"
+
+  name             = "rhel-8-x86_64"
+  ami              = "ami-038fd6fce9aaf122f"
+  instance_type    = "c5.large"
+  internal_network = var.internal_network
+}
+
+variable "internal_network" {
+  type        = bool
+  description = "Whether this instance should be in the internal network (default: false)."
+  default     = false
+}
+
+output "ip_address" {
+  value = module.aws.ip_address
+}
