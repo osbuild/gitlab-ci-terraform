75d786e792a7b58634689b24ac379678b444fa65
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2021-11-17 15:35:59 +0100

    aws: RHEL-8.5-ga runners

---
+++ aws/rhel-8.5-ga-aarch64/main.tf
@@ -0,0 +1,18 @@
+module "aws" {
+  source = "../_base"
+
+  name             = "rhel-8.5-ga-aarch64"
+  ami              = "ami-06bbacb93891d7356"
+  instance_type    = "c6g.large"
+  internal_network = var.internal_network
+}
+
+variable "internal_network" {
+  type        = bool
+  description = "Whether this instance should be in the internal network (default: false)."
+  default     = false
+}
+
+output "ip_address" {
+  value = module.aws.ip_address
+}
