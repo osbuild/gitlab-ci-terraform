1b21c6262122356deca857286ca43e1e2275f4be
Achilleas Koutsou <achilleas@koutsou.net>
2022-01-06 18:45:38 +0100

    openstack/cs9: update image
    Something went wrong with the upload of the previous image.
    Reuploaded now (and boot tested).

--- openstack/centos-stream-9-x86_64-large/main.tf
+++ openstack/centos-stream-9-x86_64-large/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "centos-stream-9"
-  image_id  = "fae9f0e7-c5ad-4bec-becb-c7d36db2b075"
+  image_id  = "9f49b07d-57e2-4998-a8e3-6d13457fde85"
   flavor_id = "c3ec7a0a-0443-4253-a6ab-040cc0278ced"
 }
 
--- openstack/centos-stream-9-x86_64/main.tf
+++ openstack/centos-stream-9-x86_64/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "centos-stream-9"
-  image_id  = "fae9f0e7-c5ad-4bec-becb-c7d36db2b075"
+  image_id  = "9f49b07d-57e2-4998-a8e3-6d13457fde85"
   flavor_id = "f2c4469b-f516-46d1-8b87-1dcca68fb3d9"
 }
 
