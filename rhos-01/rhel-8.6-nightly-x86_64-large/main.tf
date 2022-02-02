4918c081e231ac8175f97bdfce06456fa3c32761
Jakub Rusz <jrusz@redhat.com>
2022-02-02 16:14:13 +0100

    rhos-01 openstack: update 8.6 runners to our protected image

--- rhos-01/rhel-8.6-nightly-x86_64-large/main.tf
+++ rhos-01/rhel-8.6-nightly-x86_64-large/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "rhel-8-6-nightly-large"
-  image_id  = "c3ab9620-8d58-4af6-b8b6-44ee45514799"
+  image_id  = "da62f2da-ae00-4e41-91b4-88ef75c1d4f9"
   flavor_id = "c3ec7a0a-0443-4253-a6ab-040cc0278ced"
 }
 
--- rhos-01/rhel-8.6-nightly-x86_64/main.tf
+++ rhos-01/rhel-8.6-nightly-x86_64/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "rhel-8-6-nightly"
-  image_id  = "c3ab9620-8d58-4af6-b8b6-44ee45514799"
+  image_id  = "da62f2da-ae00-4e41-91b4-88ef75c1d4f9"
   flavor_id = "f2c4469b-f516-46d1-8b87-1dcca68fb3d9"
 }
 
