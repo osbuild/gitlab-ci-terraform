7918f9747637d5ecdd8cbdd963aefb31d83077a2
Ondřej Budai <ondrej@budai.cz>
2022-02-17 12:00:45 +0100

    aws: spread load across both internal subnets
    I was wondering if this is possible in pure terraform and sure it is.
    Tested locally.

--- aws/_base/data.tf
+++ aws/_base/data.tf
@@ -15,9 +15,21 @@ data "aws_subnet_ids" "internal_subnets" {
   vpc_id = data.aws_vpc.internal_vpc.id
 }
 
-# Find all of the subnet details from the internal VPC.
+# Find all of the subnets from the internal VPC.
+data "aws_subnet" "internal_subnets" {
+  for_each = data.aws_subnet_ids.internal_subnets.ids
+  id       = each.value
+}
+
+locals {
+  // convert to array so we can index this
+  // data.aws_subnet.internal_subnets is actually a map of subnets indexed by their IDs
+  internal_subnets = values(data.aws_subnet.internal_subnets)
+}
+
+# Find the primary subnet (aka the one that has the most available IP addresses)
 data "aws_subnet" "internal_subnet_primary" {
-  id = sort(data.aws_subnet_ids.internal_subnets.ids)[0]
+  id = local.internal_subnets[0].available_ip_address_count > local.internal_subnets[1].available_ip_address_count ? local.internal_subnets[0].id : local.internal_subnets[1].id
 }
 
 # Find the default VPC (not internal).
