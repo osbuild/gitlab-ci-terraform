49550279fb59a2b26a465b1c394f3afa2045005b
Alexander Todorov <atodorov@redhat.com>
2022-05-19 12:59:02 +0300

    Configure RHEL-9.1.0-20220517.0 for nightly in RHOS

--- rhos-01/rhel-9.1-nightly-x86_64-large/main.tf
+++ rhos-01/rhel-9.1-nightly-x86_64-large/main.tf
@@ -1,8 +1,8 @@
 module "openstack" {
   source = "../_base"
 
-  name      = "rhel-9-0"
-  image_id  = "dc869d35-7350-43c3-b38f-1b178ec92395"
+  name      = "rhel-9-1"
+  image_id  = "55c5d66f-c058-49e0-9e9e-16f624b0ea76"
   flavor_id = "c3ec7a0a-0443-4253-a6ab-040cc0278ced"
 }
 
