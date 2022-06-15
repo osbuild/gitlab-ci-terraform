523d70691c42911f5ddb4e16c7b6224f3e4098c9
Jakub Rusz <jrusz@redhat.com>
2022-06-15 15:37:59 +0200

    RHOS-01: Use our protected RHEL 8.7 image
    It is a guest image taken from RHEL-8.7.0-20220614.0 compose

--- rhos-01/rhel-8.7-nightly-x86_64-large/main.tf
+++ rhos-01/rhel-8.7-nightly-x86_64-large/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "rhel-8-7-nightly-large"
-  image_id  = "82e74f2b-edc4-4226-a42d-49f0a8eb70c6"
+  image_id  = "27dd121f-1512-4c1d-bed5-a717c7d5c9e2"
   flavor_id = "c3ec7a0a-0443-4253-a6ab-040cc0278ced"
 }
 
