37f8f2255c97ad9880fcf8ad023810c133a61fd1
Jakub Rusz <jrusz@redhat.com>
2022-10-12 15:00:41 +0200

    Add 8.8 and 9.2 nightly runners

---
+++ aws/rhel-8.8-nightly-x86_64/main.tf
@@ -0,0 +1,18 @@
+module "aws" {
+  source = "../_base"
+
+  name             = "rhel-8.8-nightly-x86_64"
+  ami              = "ami-02ae1f6cd70c7b0b8"
+  instance_types   = ["c6i.large", "c6a.large"]
+  internal_network = var.internal_network
+}
+
+variable "internal_network" {
+  type        = bool
+  description = "Whether this instance should be in the internal network (default: false)."
+  default     = false
+}
+
+output "ip_address" {
+  value = module.aws.ip_address
+}
