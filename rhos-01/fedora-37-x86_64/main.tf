08b8a44ca3ebb677a09e6304441d926910f3daf8
Jakub Rusz <jrusz@redhat.com>
2022-11-07 12:24:51 +0100

    rhos-01: change fedora-37 flavor to "ci"
    Fedora-37 was using "ci-ssd" machines but these do not support nested
    kvm which is necessary for our CI. Changing to "ci" enables the use of
    nested kvm.

--- rhos-01/fedora-37-x86_64/main.tf
+++ rhos-01/fedora-37-x86_64/main.tf
@@ -3,7 +3,7 @@ module "openstack" {
 
   name      = "fedora-37"
   image_id  = "edeb1b44-b086-4e61-9648-a801cc098027"
-  flavor_id = "f2c4469b-f516-46d1-8b87-1dcca68fb3d9"
+  flavor_id = "bca7d8ab-b1a4-4883-b4c6-a9536d51ebdd"
 }
 
 output "ip_address" {
