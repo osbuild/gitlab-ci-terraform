339517f2540b5a245c957228e6a3df6e1d43ca32
Alexander Todorov <atodorov@redhat.com>
2023-04-28 19:34:01 +0300

    Add RHEL-9.3.0-20230501.34 to AWS runners

---
+++ aws/rhel-9.3-nightly-aarch64/main.tf
@@ -0,0 +1,53 @@
+module "aws" {
+  source = "../_base"
+
+  name             = "rhel-9.3-nightly-aarch64"
+  ami              = "ami-0f0936e4c473c129b"
+  instance_types   = ["c7g.large", "c6gd.large", "m6gd.large"]
+  internal_network = var.internal_network
+  job_name         = var.job_name
+  project          = var.project
+  branch           = var.branch
+  pipeline_id      = var.pipeline_id
+  pipeline_source  = var.pipeline_source
+}
+
+variable "internal_network" {
+  type        = bool
+  description = "Whether this instance should be in the internal network (default: false)."
+  default     = false
+}
+
+output "ip_address" {
+  value = module.aws.ip_address
+}
+
+variable "job_name" {
+  description = "the job_name the instance is performing"
+  type        = string
+  default     = "unset"
+}
+
+variable "project" {
+  description = "the associated project"
+  type        = string
+  default     = "unset"
+}
+
+variable "branch" {
+  description = "the associated branch"
+  type        = string
+  default     = "unset"
+}
+
+variable "pipeline_id" {
+  description = "the associated pipeline_id"
+  type        = string
+  default     = "unset"
+}
+
+variable "pipeline_source" {
+  description = "the source that trigger the job"
+  type        = string
+  default     = "unset"
+}
