91382de1856824b8066806474d3b353ed6c97871
imagebuilder-bots+schutzbot@redhat.com <sanne.raymaekers@gmail.com>
2023-07-19 00:26:51 +0200

    image refresh

--- rhos-01/fedora-38-x86_64/main.tf
+++ rhos-01/fedora-38-x86_64/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "fedora-38"
-  image_id  = "23cab973-29b7-44ea-8c89-0f2d648da17b" # Fedora-Cloud-Base-38-1.6
+  image_id  = "0afed108-e9a8-4d09-8c65-786044a15dc2" # Fedora-Cloud-Base-38-1.6
   flavor_id = "bca7d8ab-b1a4-4883-b4c6-a9536d51ebdd"
 }
 
