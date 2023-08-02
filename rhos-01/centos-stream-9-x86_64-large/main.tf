4c352bb678aa27f36fe593e24943e81282abc87d
schutzbot <schutzbot@gmail.com>
2023-08-02 13:57:05 +0000

    image refresh

--- rhos-01/centos-stream-9-x86_64-large/main.tf
+++ rhos-01/centos-stream-9-x86_64-large/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "centos-stream-9"
-  image_id  = "9f49b07d-57e2-4998-a8e3-6d13457fde85"
+  image_id  = "9ed77013-a516-46b4-b9a8-1ccae9043f58"
   flavor_id = "2c6908ed-bb2b-43c7-8f9d-de790df5a4c0"
 }
 
--- rhos-01/centos-stream-9-x86_64/main.tf
+++ rhos-01/centos-stream-9-x86_64/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "centos-stream-9"
-  image_id  = "9f49b07d-57e2-4998-a8e3-6d13457fde85"
+  image_id  = "9ed77013-a516-46b4-b9a8-1ccae9043f58"
   flavor_id = "bca7d8ab-b1a4-4883-b4c6-a9536d51ebdd"
 }
 
