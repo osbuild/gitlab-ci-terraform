08c9b63451406cdb194be1f0aba9fc6428a8309a
Alexander Todorov <atodorov@redhat.com>
2023-08-10 13:48:02 +0300

    Switch to Image Builder RHEL-8.9.0-20230810.24 image

--- rhos-01/rhel-8.9-nightly-x86_64/main.tf
+++ rhos-01/rhel-8.9-nightly-x86_64/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "rhel-8-9-nightly"
-  image_id  = "2c3ac9ca-cda6-4d76-be00-8b2657fc7c24"
+  image_id  = "abfa0bc2-f34c-448a-963f-676a5ff3cae9"
   flavor_id = "bca7d8ab-b1a4-4883-b4c6-a9536d51ebdd"
 }
 
