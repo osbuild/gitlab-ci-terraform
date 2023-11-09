10a209d527184534d4fdf88f263eb55508286276
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2023-11-09 13:04:06 +0100

    aws: use general purpose instance types
    The large general purpose instance types have double the memory as
    compute optimised instances, making it less likely we see OOMs in rpm
    builds.

--- aws/fedora-36-aarch64/main.tf
+++ aws/fedora-36-aarch64/main.tf
@@ -3,7 +3,7 @@ module "aws" {
 
   name             = "fedora-36-aarch64"
   ami              = "ami-01925eb0821988986"
-  instance_types   = ["c7g.large", "c6gd.large", "m6gd.large"]
+  instance_types   = ["m6g.large", "m6gd.large", "m7g.large", "m7gd.large"]
   internal_network = var.internal_network
   job_name         = var.job_name
   project          = var.project
