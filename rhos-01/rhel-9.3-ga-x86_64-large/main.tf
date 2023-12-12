ae1d7d394e25b18a3586897efe7dcddadf096913
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2023-12-12 11:01:09 +0100

    add RHEL 8.9 and 9.3 placeholders
    These can be built by the ci image refresh bot.

---
+++ rhos-01/rhel-9.3-ga-x86_64-large/main.tf
@@ -0,0 +1,11 @@
+module "openstack" {
+  source = "../_base"
+
+  name      = "rhel-9-3-ga-large"
+  image_id  = "ad07fa2e-4c90-4360-b596-8ca6be29dee1"
+  flavor_id = "2c6908ed-bb2b-43c7-8f9d-de790df5a4c0"
+}
+
+output "ip_address" {
+  value = module.openstack.ip_address
+}
