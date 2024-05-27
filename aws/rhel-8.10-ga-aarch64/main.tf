195cb0367da282dfa2c7f871c01b61f0e534229a
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2024-05-27 16:22:57 +0200

    add RHEL 8.10 and 9.4 placeholders
    These can be built by the ci image refresh bot.

---
+++ aws/rhel-8.10-ga-aarch64/main.tf
@@ -0,0 +1,60 @@
+module "aws" {
+  source = "../_base"
+
+  name                 = "rhel-8.10-ga-aarch64"
+  ami                  = "ami-placeholder"
+  instance_types       = ["m6g.large", "m6gd.large", "m7g.large", "m7gd.large"]
+  internal_network     = var.internal_network
+  job_name             = var.job_name
+  project              = var.project
+  branch               = var.branch
+  pipeline_id          = var.pipeline_id
+  pipeline_source      = var.pipeline_source
+  iam_instance_profile = var.iam_instance_profile
+}
+
+variable "internal_network" {
+  type        = bool
+  description = "Whether this instance should be in the internal network (default: false)."
+  default     = false
+}
+
+output "ip_address" {
+  value = module.aws.ip_address
+}
+
+variable "job_name" {
+  description = "the job_name the instance is performing"
+  type        = string
+  default     = "unset"
+}
+
+variable "project" {
+  description = "the associated project"
+  type        = string
+  default     = "unset"
+}
+
+variable "branch" {
+  description = "the associated branch"
+  type        = string
+  default     = "unset"
+}
+
+variable "pipeline_id" {
+  description = "the associated pipeline_id"
+  type        = string
+  default     = "unset"
+}
+
+variable "pipeline_source" {
+  description = "the source that trigger the job"
+  type        = string
+  default     = "unset"
+}
+
+variable "iam_instance_profile" {
+  description = "instance profile to attach to the runner, the profile must exist"
+  type        = string
+  default     = null
+}
