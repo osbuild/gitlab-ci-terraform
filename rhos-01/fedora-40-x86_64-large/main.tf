920a283745dc41ff5dc7070c94b2de597ccab647
schutzbot <schutzbot@gmail.com>
2024-06-03 14:01:39 +0000

    image refresh

--- rhos-01/fedora-40-x86_64-large/main.tf
+++ rhos-01/fedora-40-x86_64-large/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "fedora-40"
-  image_id  = "placeholder"
+  image_id  = "c58bd1bc-7f05-4123-8a51-bde2996550a5"
   flavor_id = "2c6908ed-bb2b-43c7-8f9d-de790df5a4c0"
 }
 
--- rhos-01/fedora-40-x86_64/main.tf
+++ rhos-01/fedora-40-x86_64/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "fedora-40"
-  image_id  = "placeholder"
+  image_id  = "c58bd1bc-7f05-4123-8a51-bde2996550a5"
   flavor_id = "bca7d8ab-b1a4-4883-b4c6-a9536d51ebdd"
 }
 
