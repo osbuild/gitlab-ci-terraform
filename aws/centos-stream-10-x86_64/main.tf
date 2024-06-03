ef4633e8c0b2b5c45c839d3e3ac0cc91061350ba
Alexander Todorov <atodorov@redhat.com>
2024-06-03 16:39:41 +0300

    Update AWS CentOS Stream 10 runners
    with the CentOS-Stream-10-20240531.0 snapshot

--- aws/centos-stream-10-x86_64/main.tf
+++ aws/centos-stream-10-x86_64/main.tf
@@ -2,7 +2,7 @@ module "aws" {
   source = "../_base"
 
   name                 = "centos-stream-10-x86_64"
-  ami                  = "placeholder"
+  ami                  = "ami-04120d95bbaab7e95"
   instance_types       = ["m5.large", "m5d.large", "m5a.large", "m5ad.large", "m6a.large", "m6i.large", "m6id.large", "m7i.large", "m7a.large"]
   internal_network     = var.internal_network
   job_name             = var.job_name
