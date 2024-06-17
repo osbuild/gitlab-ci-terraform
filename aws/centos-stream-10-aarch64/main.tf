597413320b2dc03224290e84bff1a6c51e88f2ee
Alexander Todorov <atodorov@redhat.com>
2024-06-17 13:43:33 +0300

    Update runners to CentOS-Stream-10-20240612.0

--- aws/centos-stream-10-aarch64/main.tf
+++ aws/centos-stream-10-aarch64/main.tf
@@ -2,7 +2,7 @@ module "aws" {
   source = "../_base"
 
   name                 = "centos-stream-10-aarch64"
-  ami                  = "ami-0ee5b0d4bec02d83e"
+  ami                  = "ami-037e7a7a01c235e38"
   instance_types       = ["m6g.large", "m6gd.large", "m7g.large", "m7gd.large"]
   internal_network     = var.internal_network
   job_name             = var.job_name
