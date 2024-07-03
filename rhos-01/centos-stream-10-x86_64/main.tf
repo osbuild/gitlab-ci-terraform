2cea82f7bdc857de57c2abca34fc521f848d679d
Alexander Todorov <atodorov@redhat.com>
2024-07-03 14:07:25 +0300

    Update to CentOS-Stream-10-20240703.2

--- rhos-01/centos-stream-10-x86_64/main.tf
+++ rhos-01/centos-stream-10-x86_64/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "centos-stream-10"
-  image_id  = "439a0ba8-f011-4915-9682-6f1cd922d9c7"
+  image_id  = "1a7b6114-5ed0-448f-90fa-ef5fc263b968"
   flavor_id = "bca7d8ab-b1a4-4883-b4c6-a9536d51ebdd"
 }
 
