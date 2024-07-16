455c483a840adc6a65c47837a54bb60e5129aee4
Tomáš Hozza <thozza@redhat.com>
2024-07-16 12:59:35 +0200

    Add RHEL-10.0-nightly placeholders
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

---
+++ rhos-01/rhel-10.0-nightly-x86_64-large/main.tf
@@ -0,0 +1,11 @@
+module "openstack" {
+  source = "../_base"
+
+  name      = "rhel-10-0-nightly-large"
+  image_id  = "placeholder"
+  flavor_id = "2c6908ed-bb2b-43c7-8f9d-de790df5a4c0"
+}
+
+output "ip_address" {
+  value = module.openstack.ip_address
+}
---
+++ rhos-01/rhel-10.0-nightly-x86_64/main.tf
@@ -0,0 +1,11 @@
+module "openstack" {
+  source = "../_base"
+
+  name      = "rhel-10-0-nightly"
+  image_id  = "placeholder"
+  flavor_id = "bca7d8ab-b1a4-4883-b4c6-a9536d51ebdd"
+}
+
+output "ip_address" {
+  value = module.openstack.ip_address
+}
