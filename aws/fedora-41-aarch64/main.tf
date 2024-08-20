961f6cb4e780da441964297df7e0e6b7e7ca4cf1
schutzbot <schutzbot@gmail.com>
2024-08-20 12:56:20 +0000

    image refresh

--- aws/fedora-41-aarch64/main.tf
+++ aws/fedora-41-aarch64/main.tf
@@ -2,7 +2,7 @@ module "aws" {
   source = "../_base"
 
   name                 = "fedora-41-aarch64"
-  ami                  = "ami-06280fec6f76ad14d"
+  ami                  = "ami-01ed02a494182b84e"
   instance_types       = ["m6g.large", "m6gd.large", "m7g.large", "m7gd.large"]
   internal_network     = var.internal_network
   job_name             = var.job_name
--- rhos-01/fedora-41-x86_64/main.tf
+++ rhos-01/fedora-41-x86_64/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "fedora-41"
-  image_id  = "a6464ee9-90ad-43f9-8972-5abd1d5ff2dc"
+  image_id  = "3b9ecc9c-6aa0-4317-8a13-6978379346d1"
   flavor_id = "bca7d8ab-b1a4-4883-b4c6-a9536d51ebdd"
 }
 
