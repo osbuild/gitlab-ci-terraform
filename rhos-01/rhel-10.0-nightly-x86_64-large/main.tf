16124618cfc0d46870225a6ee933f3491405c646
schutzbot <schutzbot@gmail.com>
2024-11-26 15:58:49 +0000

    image refresh

--- rhos-01/rhel-10.0-nightly-x86_64-large/main.tf
+++ rhos-01/rhel-10.0-nightly-x86_64-large/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "rhel-10-0-nightly-large"
-  image_id  = "2225720c-5c15-489a-9cd9-8ae96c45dc7d"
+  image_id  = "2a813e51-6493-4220-96cc-8151d91999ee"
   flavor_id = "2c6908ed-bb2b-43c7-8f9d-de790df5a4c0"
 }
 
--- rhos-01/rhel-10.0-nightly-x86_64/main.tf
+++ rhos-01/rhel-10.0-nightly-x86_64/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "rhel-10-0-nightly"
-  image_id  = "2225720c-5c15-489a-9cd9-8ae96c45dc7d"
+  image_id  = "2a813e51-6493-4220-96cc-8151d91999ee"
   flavor_id = "bca7d8ab-b1a4-4883-b4c6-a9536d51ebdd"
 }
 
