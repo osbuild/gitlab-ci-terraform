48376e925fd9dd800b7bcbb347c3774507ab93d6
Tomáš Hozza <thozza@redhat.com>
2024-12-31 09:41:17 +0100

    Fedora-41/AWS: use ci-fedora-41-refresh-01-12-2024 AMIs
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- aws/fedora-41-aarch64/main.tf
+++ aws/fedora-41-aarch64/main.tf
@@ -2,7 +2,7 @@ module "aws" {
   source = "../_base"
 
   name                 = "fedora-41-aarch64"
-  ami                  = "ami-0d76caf7a259cba72"
+  ami                  = "ami-00a9a97a49e3facd7"
   instance_types       = ["m6g.large", "m6gd.large", "m7g.large", "m7gd.large"]
   internal_network     = var.internal_network
   job_name             = var.job_name
