9052ba1091bbdbb98eb2d9060b69c3fb2200185b
schutzbot <schutzbot@gmail.com>
2025-02-28 09:41:12 +0000

    image refresh

--- rhos-01/rhel-9.5-ga-x86_64-large/main.tf
+++ rhos-01/rhel-9.5-ga-x86_64-large/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "rhel-9-5-ga-large"
-  image_id  = "placeholder"
+  image_id  = "0039df68-6aea-47a5-8c53-186aedb4051d"
   flavor_id = "2c6908ed-bb2b-43c7-8f9d-de790df5a4c0"
 }
 
--- rhos-01/rhel-9.5-ga-x86_64/main.tf
+++ rhos-01/rhel-9.5-ga-x86_64/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "rhel-9-5-ga"
-  image_id  = "placeholder"
+  image_id  = "0039df68-6aea-47a5-8c53-186aedb4051d"
   flavor_id = "bca7d8ab-b1a4-4883-b4c6-a9536d51ebdd"
 }
 
