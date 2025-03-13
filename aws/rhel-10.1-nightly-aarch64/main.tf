fb938d1dadd0e7b80b67cce05358a97ad09f872e
schutzbot <schutzbot@gmail.com>
2025-03-13 17:00:42 +0000

    image refresh

--- aws/rhel-10.1-nightly-aarch64/main.tf
+++ aws/rhel-10.1-nightly-aarch64/main.tf
@@ -2,7 +2,7 @@ module "aws" {
   source = "../_base"
 
   name                 = "rhel-10.1-nightly-aarch64"
-  ami                  = "placeholder"
+  ami                  = "ami-00eb7ea83c21a0b0a"
   instance_types       = ["m6g.large", "m6gd.large", "m7g.large", "m7gd.large"]
   internal_network     = var.internal_network
   job_name             = var.job_name
