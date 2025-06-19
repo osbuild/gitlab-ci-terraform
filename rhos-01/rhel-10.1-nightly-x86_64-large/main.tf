d392fee6f47aad5040fb59f2911badb4aedcf6b7
schutzbot <schutzbot@gmail.com>
2025-06-19 20:34:56 +0000

    image refresh

--- rhos-01/rhel-10.1-nightly-x86_64-large/main.tf
+++ rhos-01/rhel-10.1-nightly-x86_64-large/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "rhel-10-1-nightly-large"
-  image_id  = "18ccce5d-c195-45c3-96cb-a7e120b7b6f1"
+  image_id  = "009c62e2-2726-4f45-ae93-a5f96c6851fc"
   flavor_id = "2c6908ed-bb2b-43c7-8f9d-de790df5a4c0"
 }
 
