4601f97adb4252bb2bf28d2dfe3f068a699aeb7a
schutzbot <schutzbot@gmail.com>
2025-06-25 08:31:43 +0000

    image refresh

--- aws/rhel-10.0-ga-aarch64/main.tf
+++ aws/rhel-10.0-ga-aarch64/main.tf
@@ -2,7 +2,7 @@ module "aws" {
   source = "../_base"
 
   name                 = "rhel-10.0-ga-aarch64"
-  ami                  = "placeholder"
+  ami                  = "ami-011e2f31503bc98fb"
   instance_types       = ["m6g.large", "m6gd.large", "m7g.large", "m7gd.large"]
   internal_network     = var.internal_network
   job_name             = var.job_name
--- rhos-01/rhel-10.0-ga-x86_64-large/main.tf
+++ rhos-01/rhel-10.0-ga-x86_64-large/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "rhel-10-0-ga-large"
-  image_id  = "placeholder"
+  image_id  = "00c711a8-94f0-4095-b13d-b7eb50c8a4d3"
   flavor_id = "2c6908ed-bb2b-43c7-8f9d-de790df5a4c0"
 }
 
