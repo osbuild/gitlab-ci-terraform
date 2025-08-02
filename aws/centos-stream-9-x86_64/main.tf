04ebcacd917b07415297b4a273624b202e354254
schutzbot <schutzbot@gmail.com>
2025-08-02 00:21:45 +0000

    image refresh

--- aws/centos-stream-9-x86_64/main.tf
+++ aws/centos-stream-9-x86_64/main.tf
@@ -2,7 +2,7 @@ module "aws" {
   source = "../_base"
 
   name                 = "centos-stream-9-x86_64"
-  ami                  = "ami-003b7cbfc2ffb6eaf"
+  ami                  = "ami-0b0cc440139dc4d2f"
   instance_types       = ["m5.large", "m5d.large", "m5a.large", "m5ad.large", "m6a.large", "m6i.large", "m6id.large", "m7i.large", "m7a.large"]
   internal_network     = var.internal_network
   job_name             = var.job_name
--- rhos-01/centos-stream-10-x86_64/main.tf
+++ rhos-01/centos-stream-10-x86_64/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "centos-stream-10"
-  image_id  = "1a7b6114-5ed0-448f-90fa-ef5fc263b968"
+  image_id  = "5c4405aa-e744-4ba6-9ea4-d11413d990db"
   flavor_id = "bca7d8ab-b1a4-4883-b4c6-a9536d51ebdd"
 }
 
