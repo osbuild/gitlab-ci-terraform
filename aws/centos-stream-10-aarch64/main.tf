ced0c67f9527e0deec254a351c958cb9c27c56a8
schutzbot <schutzbot@gmail.com>
2025-09-01 23:54:38 +0000

    image refresh

--- aws/centos-stream-10-aarch64/main.tf
+++ aws/centos-stream-10-aarch64/main.tf
@@ -2,7 +2,7 @@ module "aws" {
   source = "../_base"
 
   name                 = "centos-stream-10-aarch64"
-  ami                  = "ami-0257ed5e366145afa"
+  ami                  = "ami-0d9609638c19bdbd5"
   instance_types       = ["m6g.large", "m6gd.large", "m7g.large", "m7gd.large"]
   internal_network     = var.internal_network
   job_name             = var.job_name
