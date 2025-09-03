196e619180d6bf1215e82a261c95367be151cde9
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2025-09-03 20:15:57 +0200

    rhos-01: correct small mistakes in 10.2 and 9.8 nightlies

--- rhos-01/rhel-10.2-nightly-x86_64-large/main.tf
+++ rhos-01/rhel-10.2-nightly-x86_64-large/main.tf
@@ -1,9 +1,9 @@
 module "openstack" {
   source = "../_base"
 
-  name      = "rhel-10-1-nightly"
+  name      = "rhel-10-2-nightly-large"
   image_id  = "789c384d-4935-4c7e-8a24-a80a718f85b1"
-  flavor_id = "bca7d8ab-b1a4-4883-b4c6-a9536d51ebdd"
+  flavor_id = "2c6908ed-bb2b-43c7-8f9d-de790df5a4c0"
 }
 
 output "ip_address" {
--- rhos-01/rhel-10.2-nightly-x86_64/main.tf
+++ rhos-01/rhel-10.2-nightly-x86_64/main.tf
@@ -1,7 +1,7 @@
 module "openstack" {
   source = "../_base"
 
-  name      = "rhel-10-1-nightly"
+  name      = "rhel-10-2-nightly"
   image_id  = "789c384d-4935-4c7e-8a24-a80a718f85b1"
   flavor_id = "bca7d8ab-b1a4-4883-b4c6-a9536d51ebdd"
 }
