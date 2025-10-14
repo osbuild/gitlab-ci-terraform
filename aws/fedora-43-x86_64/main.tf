89bc37d30848c844923649ac2f01a093d6d1a439
schutzbot <schutzbot@gmail.com>
2025-10-14 16:11:37 +0000

    image refresh

--- aws/fedora-43-x86_64/main.tf
+++ aws/fedora-43-x86_64/main.tf
@@ -2,7 +2,7 @@ module "aws" {
   source = "../_base"
 
   name                 = "fedora-43-x86_64"
-  ami                  = "ami-0398f5f8b3894d792"
+  ami                  = "ami-03b7677f12575028d"
   instance_types       = ["m5.large", "m5d.large", "m5a.large", "m5ad.large", "m6a.large", "m6i.large", "m6id.large", "m7i.large", "m7a.large"]
   internal_network     = var.internal_network
   job_name             = var.job_name
