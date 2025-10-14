61492cf087370346c30d147967bf80cf93f481b4
schutzbot <schutzbot@gmail.com>
2025-10-14 21:32:16 +0000

    image refresh

--- rhos-01/fedora-43-x86_64/main.tf
+++ rhos-01/fedora-43-x86_64/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "fedora-43"
-  image_id  = "573973f2-a4cd-4ad7-b05d-a2cbe979a374"
+  image_id  = "838c6e72-f71d-4597-886b-352b3fc5610a"
   flavor_id = "bca7d8ab-b1a4-4883-b4c6-a9536d51ebdd"
 }
 
