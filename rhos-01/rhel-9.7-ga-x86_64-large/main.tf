6c9ca88db42306371a0459dea7419839abe8f736
Achilleas Koutsou <achilleas@koutsou.net>
2026-03-02 13:56:27 +0100

    Add runners for RHEL 9.7 GA
    Add new runner configurations for 9.7 GA and remove the nightlies.

--- rhos-01/rhel-9.7-ga-x86_64-large/main.tf
+++ rhos-01/rhel-9.7-ga-x86_64-large/main.tf
@@ -1,8 +1,8 @@
 module "openstack" {
   source = "../_base"
 
-  name      = "rhel-9-7-nightly-large"
-  image_id  = "1bd39b73-1353-4e96-9079-0d55428db4d6"
+  name      = "rhel-9-7-ga-large"
+  image_id  = "placeholder"
   flavor_id = "2c6908ed-bb2b-43c7-8f9d-de790df5a4c0"
 }
 
--- rhos-01/rhel-9.7-ga-x86_64/main.tf
+++ rhos-01/rhel-9.7-ga-x86_64/main.tf
@@ -1,8 +1,8 @@
 module "openstack" {
   source = "../_base"
 
-  name      = "rhel-9-7-nightly"
-  image_id  = "1bd39b73-1353-4e96-9079-0d55428db4d6"
+  name      = "rhel-9-7-ga"
+  image_id  = "placeholder"
   flavor_id = "bca7d8ab-b1a4-4883-b4c6-a9536d51ebdd"
 }
 
