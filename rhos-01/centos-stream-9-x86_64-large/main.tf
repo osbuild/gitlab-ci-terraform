c3e74b99093b771eea1b8286739af071a5e816a9
schutzbot <schutzbot@gmail.com>
2026-03-03 20:34:10 +0000

    image refresh

--- rhos-01/centos-stream-9-x86_64-large/main.tf
+++ rhos-01/centos-stream-9-x86_64-large/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "centos-stream-9-large"
-  image_id  = "46db5716-c810-4f63-a87d-eb68be480496"
+  image_id  = "e4400550-7c25-4a39-9656-47d9f17ca5b3"
   flavor_id = "2c6908ed-bb2b-43c7-8f9d-de790df5a4c0"
 }
 
--- rhos-01/centos-stream-9-x86_64/main.tf
+++ rhos-01/centos-stream-9-x86_64/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "centos-stream-9"
-  image_id  = "46db5716-c810-4f63-a87d-eb68be480496"
+  image_id  = "e4400550-7c25-4a39-9656-47d9f17ca5b3"
   flavor_id = "bca7d8ab-b1a4-4883-b4c6-a9536d51ebdd"
 }
 
