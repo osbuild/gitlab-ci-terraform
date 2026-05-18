94ccd2abf21595ed82cc96041f69500c237f116f
schutzbot <schutzbot@gmail.com>
2026-05-18 22:16:18 +0000

    image refresh

--- rhos-01/fedora-42-x86_64-large/main.tf
+++ rhos-01/fedora-42-x86_64-large/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "fedora-42-large"
-  image_id  = "dd640070-e986-400e-a67e-0e599c300718"
+  image_id  = "a9b8d47a-fadf-4466-aef0-8db851220534"
   flavor_id = "2c6908ed-bb2b-43c7-8f9d-de790df5a4c0"
 }
 
