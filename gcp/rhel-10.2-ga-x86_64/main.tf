f597cdf0033cc98bf549bc26d9cb35c4d846e8ac
schutzbot <schutzbot@gmail.com>
2026-05-29 14:29:16 +0000

    image refresh

--- gcp/rhel-10.2-ga-x86_64/main.tf
+++ gcp/rhel-10.2-ga-x86_64/main.tf
@@ -1,6 +1,6 @@
 module "google" {
   source           = "../_base"
-  image            = "ci-rhel-10-refresh-21-05-2026"
+  image            = "ci-rhel-10-refresh-29-05-2026"
   machine_type     = "n2-standard-4"
   internal_network = var.internal_network
 }
