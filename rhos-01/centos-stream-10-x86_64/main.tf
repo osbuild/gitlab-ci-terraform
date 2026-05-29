1b8377b2c2caf5675f4b895365bc03220d83a19b
schutzbot <schutzbot@gmail.com>
2026-05-29 11:01:56 +0000

    image refresh

--- rhos-01/centos-stream-10-x86_64/main.tf
+++ rhos-01/centos-stream-10-x86_64/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "centos-stream-10"
-  image_id  = "91ddf501-e452-4d19-b8c3-0dcfcb266fa0"
+  image_id  = "4e90ac0b-e119-486f-abb2-d4d57e9f0c44"
   flavor_id = "bca7d8ab-b1a4-4883-b4c6-a9536d51ebdd"
 }
 
