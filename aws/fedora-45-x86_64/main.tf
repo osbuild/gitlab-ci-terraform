01a921d7e5f30d6c7fb22abb1fcd662257cb4d33
Achilleas Koutsou <achilleas@koutsou.net>
2026-07-09 12:07:40 +0200

    Add Fedora 45 configs

---
+++ aws/fedora-45-x86_64/main.tf
@@ -0,0 +1,60 @@
+module "aws" {
+  source = "../_base"
+
+  name                 = "fedora-45-x86_64"
+  ami                  = "<placeholder>"
+  instance_types       = ["t3.large", "m8i-flex.large", "m8azn.large", "m6idn.large", "m5dn.large", "t2.large", "m8id.large", "m5zn.large", "m5d.large", "r5n.large", "r5b.large", "r5a.large", "r5d.large"]
+  internal_network     = var.internal_network
+  job_name             = var.job_name
+  project              = var.project
+  branch               = var.branch
+  pipeline_id          = var.pipeline_id
+  pipeline_source      = var.pipeline_source
+  iam_instance_profile = var.iam_instance_profile
+}
+
+variable "internal_network" {
+  type        = bool
+  description = "Whether this instance should be in the internal network (default: false)."
+  default     = false
+}
+
+output "ip_address" {
+  value = module.aws.ip_address
+}
+
+variable "job_name" {
+  description = "the job_name the instance is performing"
+  type        = string
+  default     = "unset"
+}
+
+variable "project" {
+  description = "the associated project"
+  type        = string
+  default     = "unset"
+}
+
+variable "branch" {
+  description = "the associated branch"
+  type        = string
+  default     = "unset"
+}
+
+variable "pipeline_id" {
+  description = "the associated pipeline_id"
+  type        = string
+  default     = "unset"
+}
+
+variable "pipeline_source" {
+  description = "the source that trigger the job"
+  type        = string
+  default     = "unset"
+}
+
+variable "iam_instance_profile" {
+  description = "instance profile to attach to the runner, the profile must exist"
+  type        = string
+  default     = null
+}
